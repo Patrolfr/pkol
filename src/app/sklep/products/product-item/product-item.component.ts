30d0cce5354ac60062ea82720610e99349e90e58
patrolfr <patrolwiking@gmail.com>
2020-05-16 11:09:54 +0200

    #development/product-item-design add random number of stars/reviews; addToBucket from productItemComponent;

--- src/app/sklep/products/product-item/product-item.component.ts
+++ src/app/sklep/products/product-item/product-item.component.ts
@@ -1,19 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Injectable, Input, OnInit} from '@angular/core';
 import {Product} from '../../domain/model/product.model';
+import {Store} from '@ngrx/store';
+import {AddProduct} from '../../ngRx-store/bucket.actions';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss']
 })
+@Injectable()
 export class ProductItemComponent implements OnInit {
 
   @Input() product: Product;
 
-  constructor() {
+  stars = [];
+  votes = 0;
+
+  constructor(private store: Store) {
   }
 
   ngOnInit(): void {
+    this.stars = Array(Math.floor(3 + Math.random() * 5));
+    this.votes = Math.floor(5 + Math.random() * 50);
+  }
+
+  addToBucket() {
+    this.store.dispatch(new AddProduct({amount: 1, product: this.product}));
   }
 
 }
