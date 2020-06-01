1d50c2b39b48e31e18347d74c1b39bf468f7d3d6
patrolfr <patrolwiking@gmail.com>
2020-06-01 21:37:12 +0200

    #development/clear-bucket default one item by default

--- src/app/sklep/products/product-details/product-details.component.ts
+++ src/app/sklep/products/product-details/product-details.component.ts
@@ -1,10 +1,10 @@
-import {Component, Injectable, OnInit, ViewChild} from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
 import {Product} from '../../domain/model/product.model';
 import {ActivatedRoute, Params} from '@angular/router';
 import {ProductService} from '../../service/product.service';
 import {Store} from '@ngrx/store';
 import {AddProduct} from '../../ngRx-store/bucket.actions';
-import {FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthenticationService} from '../../service/authentication.service';
 import {Review} from '../../domain/model/review.model';
 import {ReviewService} from '../../service/review.service';
@@ -18,12 +18,11 @@ import {BucketEventsEmitter} from '../../service/bucket-events-emitter';
 @Injectable()
 export class ProductDetailsComponent implements OnInit {
 
-  @ViewChild('productsAmountForm') productsAmountForm: NgForm;
+  inputForm: FormGroup;
   public product: Product;
   public reviews: Review[] = [];
   reviewForm: FormGroup;
   submitted = false;
-  public newReview: Review;
 
   constructor(private route: ActivatedRoute,
               public authenticationService: AuthenticationService,
@@ -35,10 +34,14 @@ export class ProductDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const defaultProductAmount = 1;
+    this.inputForm = new FormGroup({
+      productAmount: new FormControl(defaultProductAmount, Validators.pattern('^[1-9]+[0-9]*$')),
+    });
+
     this.route.params.subscribe((params: Params) => {
       const productId = Number(params.id); // handling so called type safety of Type Script..
       this.product = this.productService.getProductById(productId);
-      console.log(this.product);
     });
 
     this.reviewService.getProductReviews(this.product.id).subscribe(data => {
@@ -48,11 +51,8 @@ export class ProductDetailsComponent implements OnInit {
           rating: element.rating,
           comment: element.comment,
         };
-
         this.reviews.push(review);
-
       }
-      console.log(this.reviews);
     });
 
     if (this.authenticationService.isLogged()){
@@ -78,9 +78,6 @@ export class ProductDetailsComponent implements OnInit {
       comment: this.reviewForm.value.comment,
     };
 
-
-    console.log(data);
-
     this.reviewService.sendReview(data).subscribe(response => {
       window.alert('Comment added.');
       window.location.reload();
@@ -89,22 +86,13 @@ export class ProductDetailsComponent implements OnInit {
       });
   }
 
-
   onSubmitItem() {
-    this.addToBucket(this.productsAmountForm.value.amount);
+    this.addToBucket(this.inputForm.value.productAmount);
   }
 
   addToBucket(amountOfProducts: number = 1) {
     const addEvent = new AddProduct({amount: amountOfProducts, product: this.product});
     this.store.dispatch(addEvent);
     this.emitter.emit(addEvent);
-  }
-
-  sendReview(data) {
-    return this.reviewService.sendReview(data).subscribe(response => {
-      window.location.reload();
-    }, error => {
-      window.alert('Błąd wysyłki opinii.');
-    });
   }
 }
