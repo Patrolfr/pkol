90bfa3f3e4b9aaa2fb8a23dc42977433fa19a5b3
patrolfr <patrolwiking@gmail.com>
2021-01-13 21:07:01 +0100

    - Add pkoleasing script element to AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'sklepfrontend';
+
+  ngOnInit(): void {
+    const node = document.createElement('script');
+    node.src = 'https://stpc.pkoleasing.pl/leasing/assets/widget/pkol-installment-widget.js';
+    node.type = 'text/javascript';
+    node.async = false;
+    node.charset = 'utf-8';
+    document.getElementsByTagName('body')[0].appendChild(node);
+  }
 }
