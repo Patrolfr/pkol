cad05e4b2ca508b9c98e094b9cfcf8314abdfda7
Błażej Szczepanowski <blazej1679@gmail.com>
2021-01-19 20:39:01 +0100

    Fix computing total products value for leasing widget.

--- src/app/sklep/common/leasing-widget/leasing-widget.component.ts
+++ src/app/sklep/common/leasing-widget/leasing-widget.component.ts
@@ -10,7 +10,7 @@ export class LeasingWidgetComponent implements OnInit {
   API_URL = "https://stpc.pkoleasing.pl";
   LEASING_FORM_ID = "pko-leasing-form";
   SHOP_ID = "573278292"
-  @Input() products: Product[];
+  @Input() products: Product[] | undefined;
   @Input() widget_style = "1";
   @Input() source = "item";
   unique_product_quantity = '1';
@@ -18,15 +18,23 @@ export class LeasingWidgetComponent implements OnInit {
   total_value = 0;
   total_net_value = 0;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+
     this.unique_product_quantity = String(this.products.length);
 
     for (const product of this.products) {
-      this.total_value += Number(product.total_value) * Number(product.product_quantity);
-      this.total_net_value = Number(product.net_value) * Number(product.product_quantity);
+
+      this.total_value += (+product.total_value * +product.product_quantity);
+      this.total_net_value += (+product.net_value * +product.product_quantity);
     }
+
+
+    console.log("WIDGEEEEEET");
+    console.log(this.total_net_value);
+    console.log(this.total_value);
 
     this.ifRenderFlag = true;
   }
