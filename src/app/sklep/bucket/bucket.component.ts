378f903ace39bb56e202680c68ed6a1165bb5614
patrolfr <patrolwiking@gmail.com>
2021-01-24 13:21:37 +0100

    - Display pkol widget in bucket for multiple products; - Round product values to 2 digits.

--- src/app/sklep/bucket/bucket.component.ts
+++ src/app/sklep/bucket/bucket.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {AppState, BucketEntry} from '../ngRx-store/user-bucket.reducers';
+import {AppState, BucketEntry, bucketReducer} from '../ngRx-store/user-bucket.reducers';
 import {Store} from '@ngrx/store';
 import {RemoveProduct} from '../ngRx-store/bucket.actions';
 import {Observable, Subscription} from 'rxjs';
 import {AuthenticationService} from '../service/authentication.service';
 import {BucketEventsEmitter} from '../service/bucket-events-emitter';
+import {Product} from '../domain/model/product.model';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-bucket',
@@ -15,6 +17,7 @@ export class BucketComponent implements OnInit, OnDestroy {
 
   storeSubscription: Subscription;
   bucketEntries: Observable<BucketEntry[]>;
+  products: Observable<Product[]>;
   totalCostInPln: Observable<number>;
   totalDiscountInPln: Observable<number>;
 
@@ -27,6 +30,10 @@ export class BucketComponent implements OnInit, OnDestroy {
     this.bucketEntries = this.store.select('bucketState', 'entries');
     this.totalCostInPln = this.store.select('bucketState', 'totalCostInPln');
     this.totalDiscountInPln = this.store.select('bucketState', 'totalDiscountInPln');
+    this.products = this.bucketEntries.pipe(map( entries => {
+      const products = entries.map(entry => Array(entry.amount).fill(entry.product));
+      return [].concat(...products);
+    }));
   }
 
   onRemoveProduct(productId: number) {
--- src/app/sklep/common/leasing-widget/leasing-widget.component.ts
+++ src/app/sklep/common/leasing-widget/leasing-widget.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Product} from "../../domain/model/product.model";
+import {Product} from '../../domain/model/product.model';
 
 @Component({
   selector: 'app-leasing-widget',
@@ -7,12 +7,12 @@ import {Product} from "../../domain/model/product.model";
   styleUrls: ['./leasing-widget.component.scss']
 })
 export class LeasingWidgetComponent implements OnInit {
-  API_URL = "https://stpc.pkoleasing.pl";
-  LEASING_FORM_ID = "pko-leasing-form";
-  SHOP_ID = "573278292"
+  API_URL = 'https://stpc.pkoleasing.pl';
+  LEASING_FORM_ID = 'pko-leasing-form';
+  SHOP_ID = '573278292';
   @Input() products: Product[] | undefined;
-  @Input() widget_style = "1";
-  @Input() source = "item";
+  @Input() widget_style = '1';
+  @Input() source = 'item';
   unique_product_quantity = '1';
   ifRenderFlag = false;
   total_value = 0;
@@ -35,9 +35,13 @@ export class LeasingWidgetComponent implements OnInit {
       this.total_net_value += parseFloat(product.net_value.replace(',', '.')) * parseFloat(product.product_quantity);
     }
 
-    this.total_value_string = ('' + this.total_value).replace('.', ',');
-    this.total_net_value_string = ('' + this.total_net_value).replace('.', ',');
+    this.total_value_string = this.total_value.toFixed(2).replace('.', ',');
+    this.total_net_value_string = this.total_net_value.toFixed(2).replace('.', ',');
 
+    console.log('total_value_string');
+    console.log(this.total_value_string);
+    console.log('total_net_value_string');
+    console.log(this.total_net_value_string);
     this.ifRenderFlag = true;
   }
 
